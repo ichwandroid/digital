a38e09429a0648d98598d5613aedca7e02fc35b3
Ichwandroid <ichwan.laruku@gmail.com>
2023-08-02 21:21:10 +0700

    beta digital

---
+++ README.md
@@ -0,0 +1,2 @@
+# PWA Template Project
+A simple template project for PWAs with a service worker and caching.
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/8.3__iPad_Mini_portrait.png">
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/12.9__iPad_Pro_landscape.png">
+        <link rel="icon" type="image/png" href="img/favicon.png">
+        <link rel="stylesheet" href="style.css">
+        <link rel="manifest" href="manifest.json">
+        <script src="config.js"></script>
+        <script src="src/App.js"></script>
+        <script src="src/main.js"></script>
+    <title>Digital Pickup</title>
+</head>
+<body onload="init()">
+    Simple PWA Template
+</body>
+</html>
